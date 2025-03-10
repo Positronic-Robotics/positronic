fix franka position quaternion; fix teleop transforms; fix data_collection_zen

positronic/teleop.py
@@ -4,33 +4,54 @@ from typing import Callable, List
 import numpy as np
 
 import ironic as ir
-from geom import Rotation, Transform3D
+import geom
 from positronic.tools.buttons import ButtonHandler
 
 logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler(), logging.FileHandler("teleop.log", mode="w")])
 
 
-def front_position_parser(value: Transform3D) -> Transform3D:
-    pos = np.array([value.translation[2], value.translation[0], value.translation[1]])
-    quat = value.rotation.as_quat
-    quat = Rotation.from_quat([quat[0], -quat[3], -quat[1], -quat[2]])
+def front_position_franka_parser(teleop_transform: geom.Transform3D) -> geom.Transform3D:
+    """
+    This function maps the teleop transform to the franka robot frame, when operator is facing the robot
+    from the front.
 
-    # Don't ask my why these transformations, I just got them
-    # Rotate quat 90 degrees around Y axis
-    rotation_y_90 = Rotation(np.cos(np.pi / 4), 0, np.sin(np.pi / 4), 0)
-    return Transform3D(pos, quat * rotation_y_90)
+    It achieves this by permuting the axes xyz -> zxy.
+
+    Args:
+        teleop_transform: (geom.Transform3D) The input transform to be parsed.
+
+    Returns:
+        franka_transform: (geom.Transform3D) The transformed transform.
+    """
+    translation = teleop_transform.translation[[2, 0, 1]]
+    # Apply rotation to map rotation around z -> y, x -> z, y -> x
+    axis_permutation = geom.Rotation(0.5, 0.5, 0.5, 0.5)
+
+    rotation = axis_permutation * teleop_transform.rotation * axis_permutation.inv
+    return geom.Transform3D(translation, rotation)
 
 
-def back_position_parser(value: Transform3D) -> Transform3D:
-    pos = np.array([-value.translation[2], -value.translation[0], value.translation[1]])
-    quat = value.rotation.as_quat
-    quat = Rotation.from_quat([quat[0], quat[3], quat[1], quat[2]])
+def back_position_franka_parser(teleop_transform: geom.Transform3D) -> geom.Transform3D:
+    """
+    This function maps the teleop transform to the franka robot frame, when operator is facing the robot
+    from the back.
 
-    res_quat = quat
-    rotation_y_90 = Rotation(np.cos(-np.pi / 4), 0, np.sin(-np.pi / 4), 0)
-    res_quat = rotation_y_90 * quat
-    res_quat = Rotation(res_quat[0], -res_quat[1], res_quat[2], res_quat[3])
-    return Transform3D(pos, res_quat)
+    The same as front_position_franka_parser, but with additional X and Y axis flips.
+
+    Args:
+        teleop_transform: (geom.Transform3D) The input transform to be parsed.
+
+    Returns:
+        franka_transform: (geom.Transform3D) The transformed transform.
+    """
+    front_pos = front_position_franka_parser(teleop_transform)
+    translation = front_pos.translation * [-1, -1, 1]
+
+    flip_xy_rotation = geom.Rotation(0, 0, 0, 1)
+    rotation = flip_xy_rotation * front_pos.rotation * flip_xy_rotation.inv
+
+    return geom.Transform3D(translation, rotation)
+
 
 
 @ir.ironic_system(
@@ -40,7 +61,7 @@ def back_position_parser(value: Transform3D) -> Transform3D:
     output_props=["metadata"])
 class TeleopSystem(ir.ControlSystem):
 
-    def __init__(self, pos_parser: Callable[[Transform3D], Transform3D]):
+    def __init__(self, pos_parser: Callable[[geom.Transform3D], geom.Transform3D]):
         super().__init__()
         self.teleop_t = None
         self.offset = None
@@ -66,7 +87,7 @@ class TeleopSystem(ir.ControlSystem):
         self.fps.tick()
 
         if self.is_tracking and self.offset is not None:
-            target = Transform3D(self.teleop_t.translation + self.offset.translation,
+            target = geom.Transform3D(self.teleop_t.translation + self.offset.translation,
                                  self.teleop_t.rotation * self.offset.rotation)
             await self.outs.robot_target_position.write(ir.Message(target, message.timestamp))
 
@@ -100,7 +121,7 @@ class TeleopSystem(ir.ControlSystem):
         # Note that translation and rotation offsets are independent
         if self.teleop_t is not None:
             robot_t = (await self.ins.robot_position()).data
-            self.offset = Transform3D(-self.teleop_t.translation + robot_t.translation,
+            self.offset = geom.Transform3D(-self.teleop_t.translation + robot_t.translation,
                                       self.teleop_t.rotation.inv * robot_t.rotation)
         if self.is_tracking:
             logging.info('Stopped tracking')