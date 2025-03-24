39bdce373da488fab5d19f567119d9702cbef4a3
Andrew Luzan <aluzan@positronic.ro>
2025-03-24 22:05:12 +0200

    some changes

--- docker/run.sh
+++ docker/run.sh
@@ -2,6 +2,7 @@ xhost + local:docker
 
 docker run --gpus all --shm-size 128G --rm \
     --volume $PWD:/positronic \
+    --volume $PWD/../lerobot:/lerobot \
     --network=host \
     --privileged \
     -e DISPLAY=$DISPLAY \
