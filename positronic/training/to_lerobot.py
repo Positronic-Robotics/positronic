8776623c85f9263b1c187b44b9d49eb66c58aca0
Andrew Luzan <luzan.minsk@gmail.com>
2025-04-22 11:52:42 +0300

    append data

--- positronic/training/to_lerobot.py
+++ positronic/training/to_lerobot.py
@@ -93,6 +93,67 @@ def process_timestamps(
     return timestamps_seconds.unsqueeze(-1)
 
 
+def seconds_to_str(seconds: torch.Tensor) -> str:
+    seconds = seconds.item()
+    if seconds < 60:
+        return f"{seconds:.2f}s"
+    elif seconds < 3600:
+        return f"{seconds / 60:.2f}m"
+    else:
+        return f"{seconds / 3600:.2f}h"
+
+
+def append_data_to_dataset(
+    dataset: LeRobotDataset,
+    input_dir: Path,
+    fps: int,
+    start_from_zero: bool,
+    timestamp_units: str,
+    synchronize_timestamps: bool,
+    state_encoder: StateEncoder,
+    action_encoder: ActionDecoder,
+    task: str,
+):
+
+    # Process each episode file
+    episode_files = sorted([f for f in input_dir.glob('*.pt')])
+    total_length = 0
+
+    for episode_idx, episode_file in enumerate(tqdm.tqdm(episode_files, desc="Processing episodes")):
+        episode_data = torch.load(episode_file)
+
+        for key in episode_data.keys():
+            # TODO: come up with a better way to determine if the data is a video (X2 !!!)
+            if key.startswith('image.') or key.endswith('.image') and len(episode_data[key].shape) == 1:
+                episode_data[key] = _decode_video_from_array(episode_data[key])
+
+
+        obs = state_encoder.encode_episode(episode_data)
+        num_frames = len(episode_data['image_timestamp'])
+        ep_dict = {**obs}
+
+        # Concatenate all the data as specified in the config
+        ep_dict['action'] = action_encoder.encode_episode(episode_data)
+
+        timestamps = process_timestamps(
+            episode_data['image_timestamp'],
+            start_from_zero=start_from_zero,
+            timestamp_units=timestamp_units,
+            synchronize_with_fps=fps if synchronize_timestamps else None,
+        )
+        total_length += timestamps[-1] - timestamps[0]
+
+        for i in range(num_frames):
+            frame = {"task": task}
+            for key in ep_dict.keys():
+                frame[key] = ep_dict[key][i]
+            dataset.add_frame(frame)
+
+        dataset.save_episode()
+        dataset.encode_episode_videos(episode_idx)
+    print(f"Total length of the dataset: {seconds_to_str(total_length)}")
+
+
 @ir.config()
 def features():
     return {
@@ -137,7 +198,7 @@ def convert_to_lerobot_dataset(  # noqa: C901  Function is too complex
     }
 
     dataset = LeRobotDataset.create(
-        repo_id=output_dir.name,
+        repo_id='local',
         fps=fps,
         root=output_dir,
         use_videos=video,
@@ -145,82 +206,60 @@ def convert_to_lerobot_dataset(  # noqa: C901  Function is too complex
         image_writer_threads=32,
     )
 
-    # Process each episode file
-    episode_files = sorted([f for f in input_dir.glob('*.pt')])
-    total_length = 0
-
-    for episode_idx, episode_file in enumerate(tqdm.tqdm(episode_files, desc="Processing episodes")):
-        episode_data = torch.load(episode_file)
-
-        for key in episode_data.keys():
-            # TODO: come up with a better way to determine if the data is a video (X2 !!!)
-            if key.startswith('image.') or key.endswith('.image') and len(episode_data[key].shape) == 1:
-                episode_data[key] = _decode_video_from_array(episode_data[key])
-
-
-        obs = state_encoder.encode_episode(episode_data)
-        num_frames = len(episode_data['image_timestamp'])
-        ep_dict = {**obs}
-
-        # Concatenate all the data as specified in the config
-        ep_dict['action'] = action_encoder.encode_episode(episode_data)
-
-        timestamps = process_timestamps(
-            episode_data['image_timestamp'],
-            start_from_zero=start_from_zero,
-            timestamp_units=timestamp_units,
-            synchronize_with_fps=fps if synchronize_timestamps else None,
-        )
-        total_length += timestamps[-1] - timestamps[0]
-
-        for i in range(num_frames):
-            frame = {"task": task}
-            for key in ep_dict.keys():
-                frame[key] = ep_dict[key][i]
-            dataset.add_frame(frame)
-
-        dataset.save_episode()
-        dataset.encode_episode_videos(episode_idx)
-
-
-    # # Create HuggingFace dataset
-    # episode_data_index = calculate_episode_data_index(hf_dataset)
-
-    # info = {
-    #     "codebase_version": CODEBASE_VERSION,
-    #     "fps": fps,
-    #     "video": video,
-    # }
-    # if video:
-    #     info["encoding"] = get_default_encoding()
-
-    # # Create LeRobotDataset
-    # lerobot_dataset = LeRobotDataset.from_preloaded(
-    #     repo_id="local/dataset",
-    #     hf_dataset=hf_dataset,
-    #     episode_data_index=episode_data_index,
-    #     info=info,
-    #     videos_dir=output_dir / "videos",
-    # )
-
-    # if run_compute_stats:
-    #     logging.info("Computing dataset statistics")
-    #     stats = compute_stats(lerobot_dataset, batch_size=4, max_num_samples=10_000)
-    #     lerobot_dataset.stats = stats
-    # else:
-    #     stats = {}
-    #     logging.info("Skipping computation of the dataset statistics")
-
-    # # Save dataset components
-    # hf_dataset = hf_dataset.with_format(None)  # to remove transforms that can't be saved
-    # hf_dataset.save_to_disk(str(output_dir / "train"))
-
-    # meta_data_dir = output_dir / "meta_data"
-    # save_meta_data(info, stats, episode_data_index, meta_data_dir)
-
+    append_data_to_dataset(
+        dataset=dataset,
+        input_dir=input_dir,
+        fps=fps,
+        start_from_zero=start_from_zero,
+        timestamp_units=timestamp_units,
+        synchronize_timestamps=synchronize_timestamps,
+        state_encoder=state_encoder,
+        action_encoder=action_encoder,
+        task=task,
+    )
     print(f"Dataset converted and saved to {output_dir}")
-    print(f"Total length of the dataset: {total_length} seconds")
+
+@ir.config(
+    fps=30,
+    start_from_zero=True,
+    timestamp_units='ns',
+    synchronize_timestamps=True,
+    state_encoder=positronic.cfg.inference.state.end_effector,
+    action_encoder=positronic.cfg.inference.action.umi_relative,
+    task="pick plate from the table and place it into the dishwasher",
+)
+def append_data_to_lerobot_dataset(  # noqa: C901  Function is too complex
+    dataset_dir: str,
+    input_dir: Path,
+    fps: int,
+    start_from_zero: bool,
+    timestamp_units: str,
+    synchronize_timestamps: bool,
+    state_encoder: StateEncoder,
+    action_encoder: ActionDecoder,
+    task: str,
+):
+    dataset = LeRobotDataset(
+        repo_id='local',
+        root=dataset_dir,
+    )
+
+    append_data_to_dataset(
+        dataset=dataset,
+        input_dir=Path(input_dir),
+        fps=fps,
+        start_from_zero=start_from_zero,
+        timestamp_units=timestamp_units,
+        synchronize_timestamps=synchronize_timestamps,
+        state_encoder=state_encoder,
+        action_encoder=action_encoder,
+        task=task,
+    )
+    print(f"Dataset extended with {input_dir} and saved to {dataset_dir}")
 
 
 if __name__ == "__main__":
-    fire.Fire(convert_to_lerobot_dataset.override_and_instantiate)
+    fire.Fire({
+        'convert': convert_to_lerobot_dataset.override_and_instantiate,
+        'append': append_data_to_lerobot_dataset.override_and_instantiate,
+    })
