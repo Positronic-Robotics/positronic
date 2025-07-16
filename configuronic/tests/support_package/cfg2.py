5406a6877438980bbfd9c126c0fcf2cd190cc189
Andrew Luzan <luzan.minsk@gmail.com>
2025-07-16 16:04:15 +0300

    add missing file

---
+++ configuronic/tests/support_package/cfg2.py
@@ -0,0 +1,9 @@
+import configuronic as cfgc
+from configuronic.tests.support_package.cfg import a_cfg_value1
+
+a_cfg_value1_copy = a_cfg_value1.copy()
+a_cfg_value1_override_value3 = a_cfg_value1.override(value=3)
+
+@cfgc.config
+def return2():
+    return 2
