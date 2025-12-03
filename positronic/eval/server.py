376e4b83160d198581fef193fb72596292fb1529
Sergey Arkhangelskiy <mc.vertix@gmail.com>
2025-12-03 16:25:02 +0200

    Update configurations

--- positronic/eval/server.py
+++ positronic/eval/server.py
@@ -8,66 +8,25 @@ from .ds import evaluation_ds
 config = ps.main.override(
     dataset=evaluation_ds,
     episode_keys={
-        'duration': {'label': 'Duration', 'format': '%.2f sec'},
-        'task_code': {
-            'label': 'Task',
-            'filter': True,
-            'renderer': {
-                'type': 'badge',
-                'options': {
-                    'towels': {'label': 'Towels', 'variant': 'danger'},
-                    'spoons': {'label': 'Spoons', 'variant': 'warning'},
-                    'scisors': {'label': 'Scisors', 'variant': 'success'},
-                },
-            },
-        },
-        'inference.policy.name': {
-            'label': 'Policy',
-            'filter': True,
-            'renderer': {
-                'type': 'badge',
-                'options': {
-                    'act': {'label': 'ACT', 'variant': 'danger'},
-                    'gr00t': {'label': 'Groot', 'variant': 'warning'},
-                    'openpi 0.5': {'label': 'OpenPI 0.5', 'variant': 'success'},
-                },
-            },
-        },
-        'eval.external_camera': {
-            'label': 'External camera',
-            'filter': True,
-            'renderer': {
-                'type': 'badge',
-                'options': {
-                    'left': {'label': 'Left', 'variant': 'danger'},
-                    'right': {'label': 'Right', 'variant': 'warning'},
-                    # NA Should have no badge
-                },
-            },
-        },
-        'eval.tote_placement': {
-            'label': 'Source tote placement',
-            'filter': True,
-            'renderer': {
-                'type': 'badge',
-                'options': {
-                    'left': {'label': 'Left', 'variant': 'danger'},
-                    'right': {'label': 'Right', 'variant': 'warning'},
-                    # NA Should have no badge
-                },
-            },
-        },
+        # TODO: We need to have an ability to remove the first ID column
+        'task_code': {'label': 'Task', 'filter': True},
+        'model': {'label': 'Model', 'filter': True},
+        'units': {'label': 'Units'},
+        'uph': {'label': 'UPH', 'format': '%.1f'},
+        'success': {'label': 'Success', 'format': '%.1f%%'},
+        # 'started': {'label': 'Started', 'format': '%Y-%m-%d %H:%M:%S'},  # This does not work now
         'full_success': {
-            'label': 'Success',
-            'filter': True,
+            'label': 'Status',
+            'filter': True,  # Currently filter shows true / false, I expect it to show Pass / Fail
             'renderer': {
                 'type': 'badge',
                 'options': {
-                    True: {'label': 'Success', 'variant': 'success'},
-                    False: {'label': 'Failure', 'variant': 'danger'},
+                    True: {'label': 'Pass', 'variant': 'success'},
+                    False: {'label': 'Fail', 'variant': 'danger'},
                 },
             },
         },
+        'duration': {'label': 'Duration', 'format': '%.1f sec'},
     },
 )
 
