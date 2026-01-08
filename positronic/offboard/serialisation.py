2fd51f993a5eb9921ab012f32f539805c82b816c
Sergey Arkhangelskiy <mc.vertix@gmail.com>
2026-01-08 16:23:20 +0200

    Consolidate serialization imports to positronic.utils.serialization

--- positronic/offboard/serialisation.py
+++
@@ -1,8 +0,0 @@
-"""Offboard wire serialization helpers.
-
-This module re-exports from positronic.utils.serialization for backwards compatibility.
-"""
-
-from positronic.utils.serialization import deserialise, serialise
-
-__all__ = ['serialise', 'deserialise']
