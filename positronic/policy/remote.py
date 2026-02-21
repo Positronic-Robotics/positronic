a312c64d87ea7c8a088a7c001d7f2431b4838972
Sergey Arkhangelskiy <mc.vertix@gmail.com>
2026-02-21 11:48:50 +0200

    Simplify resize target resolution

--- positronic/policy/remote.py
+++ positronic/policy/remote.py
@@ -72,12 +72,8 @@ class RemotePolicy(Policy):
         for key, value in obs.items():
             if isinstance(value, np.ndarray) and value.ndim == 3 and value.shape[2] == 3:
                 target = self._image_sizes.get(key, self._default_image_size)
-                if target is not None:
-                    tw, th = target
-                elif self._resize is not None:
-                    tw = th = self._resize
-                else:
-                    tw = th = 0
+                r = self._resize or 0
+                tw, th = target or (r, r)
                 if tw > 0 and th > 0:
                     h, w = value.shape[:2]
                     scale = min(1.0, tw / w, th / h)
