d24d8b787002395986fc168f8c4351be658302bb
Sergey Arkhangelskiy <mc.vertix@gmail.com>
2026-03-16 19:40:07 +0200

    Add import guard for unsupported Kinova driver

---
+++ positronic/drivers/roboarm/kinova/__init__.py
@@ -0,0 +1,5 @@
+raise ImportError(
+    'Kinova driver is temporarily unsupported: the `kortex_api` package is unavailable '
+    '(artifactory.kinovaapps.com is unreliable). Install it manually if needed:\n'
+    '  pip install https://artifactory.kinovaapps.com/artifactory/generic-local-public/kortex/API/2.7.0/kortex_api-2.7.0.post5-py3-none-any.whl'
+)
