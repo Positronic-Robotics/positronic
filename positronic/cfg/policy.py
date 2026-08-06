2bf06ff43c5e61e385905cba9d61125eacd09ec7
Sergey Arkhangelskiy <mc.vertix@gmail.com>
2026-08-06 11:39:06 +0300

    Repoint `runway_ziyi` at the 150k gyros batch

--- positronic/cfg/policy.py
+++ positronic/cfg/policy.py
@@ -132,9 +132,9 @@ runway_anton = production.override(
 
 runway_ziyi = production.override(
     endpoints={
-        'fm112k_us': 'wss://runway-pythagoras-dev--gyros-fm112k-us-gyrosserver-web.modal.run',
-        'fm_actuni_112k': 'wss://runway-pythagoras-dev--gyros-fm-actuni-112k-gyrosserver-web.modal.run',
-        'fm_tsu_actuni_112k': 'wss://runway-pythagoras-dev--gyros-fm-tsu-actuni-112k-gyrosserver-web.modal.run',
+        'fm150k_us': 'wss://runway-pythagoras-dev--gyros-fm150k-us-gyrosserver-web.modal.run',
+        'fm_actuni_150k': 'wss://runway-pythagoras-dev--gyros-fm-actuni-150k-gyrosserver-web.modal.run',
+        'fm_tsu_actuni_150k': 'wss://runway-pythagoras-dev--gyros-fm-tsu-actuni-150k-gyrosserver-web.modal.run',
     },
     sampler=balanced,
     group_fields=EVAL_GROUP_FIELDS,
