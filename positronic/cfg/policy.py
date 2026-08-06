7eb323cf181a7f9c7f8e0bb237aba7a47244a7f3
Sergey Arkhangelskiy <mc.vertix@gmail.com>
2026-08-06 16:53:46 +0300

    Drop `nm_n60_50k`: its handshake stalls and takes the eval down

--- positronic/cfg/policy.py
+++ positronic/cfg/policy.py
@@ -134,7 +134,6 @@ runway_ziyi = production.override(
     endpoints={
         'fm150k_us': 'wss://runway-pythagoras-dev--gyros-fm150k-us-gyrosserver-web.modal.run',
         'fm_tsu_actuni_150k': 'wss://runway-pythagoras-dev--gyros-fm-tsu-actuni-150k-gyrosserver-web.modal.run',
-        'nm_n60_50k': 'wss://runway-pythagoras-dev--gyros-nm-n60-50k-gyrosserver-web.modal.run',
     },
     sampler=balanced,
     group_fields=EVAL_GROUP_FIELDS,
